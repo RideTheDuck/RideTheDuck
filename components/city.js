505b12d170906be0b6b3e7438c723b9cb0972ea5
Ara <hello.ara@pm.me>
2021-02-16 14:25:10 +0000

    api fetch now working

--- components/city.js
+++ components/city.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, Text, FlatList } from 'react-native';
+import axios from 'axios';
 
 import t from 'tcomb-form-native'; // 0.6.9
 
 const Form = t.form.Form;
 
-const User = t.struct({
-  email: t.String,
-  username: t.maybe(t.String),
-  password: t.String,
-  terms: t.Boolean
+const City = t.struct({
+  city: t.String,
+  // username: t.maybe(t.String),
+  // password: t.String,
+  // terms: t.Boolean
 });
 
 const formStyles = {
@@ -36,25 +37,53 @@ const formStyles = {
   }
 }
 
-const options = {
-  fields: {
-    email: {
-      error: 'Without an email address how are you going to reset your password when you forget it?'
-    },
-    password: {
-      error: 'Choose something you use on a dozen other sites or something you won\'t remember'
-    },
-    terms: {
-      label: 'Agree to Terms',
-    },
-  },
-  stylesheet: formStyles,
-};
+// const options = {
+//   fields: {
+//     email: {
+//       error: 'Without an email address how are you going to reset your password when you forget it?'
+//     },
+//     password: {
+//       error: 'Choose something you use on a dozen other sites or something you won\'t remember'
+//     },
+//     terms: {
+//       label: 'Agree to Terms',
+//     },
+//   },
+//   stylesheet: formStyles,
+// };
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults:[],
+      isShowingResults: false,
+    }
+  }
   handleSubmit = () => {
     const value = this._form.getValue();
-    console.log('value: ', value);
+    const cityName = value.city
+    this.setState({searchKeyword: cityName})
+    axios.get(`https://restcountries.eu/rest/v2/capital/${cityName}`)
+    .then(res=> {
+      console.log(res)
+      console.log('name')
+      console.log(res.data[0].name)
+      console.log('timezones')
+      console.log(res.data[0].timezones[0])
+      console.log("flag")
+      console.log(res.data[0].flag)
+      console.log("currency")
+      console.log(res.data[0].currencies[0].code)
+      this.setState({
+        // name: res.data[0].name,
+        // timezone: res.data[0].timezones[0],
+        // flag: '',
+        // currency: res.data[0].currencies[0].code,
+        searchResults: res.data[0],
+        isShowingResults: true
+      })
+    })
   }
   
   render() {
@@ -62,13 +91,23 @@ export default class App extends Component {
       <View style={styles.container}>
         <Form 
           ref={c => this._form = c}
-          type={User} 
-          options={options}
+          type={City} 
+          // options={options}
         />
         <Button
-          title="Sign Up!"
+          title="Enter"
           onPress={this.handleSubmit}
         />
+        {this.state.isShowingResults && (
+            <FlatList
+              data={this.state.searchResults}
+              renderItem={item => {
+                return (
+                    <Text>{item}</Text>
+                );
+              }}
+            />
+          )}
       </View>
     );
   }
