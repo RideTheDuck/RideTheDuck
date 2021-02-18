5872304f521dec6a47f5e12c1cb6744833ae17c6
xavierloos <xavier.loos04@gmail.com>
2021-02-18 09:50:11 +0000

    Search

--- components/city.js
+++ components/city.js
@@ -18,6 +18,8 @@ let currency;
 let name;
 let language;
 let capital;
+// const [term,setTerm] = useState('')
+// const [results, setResults] = useState([])
 const Form = t.form.Form;
 
 const CitySearch = t.struct({
@@ -34,6 +36,7 @@ const options = {
 }
 
 export default class City extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
@@ -48,38 +51,53 @@ export default class City extends Component {
     const cityName = this.state.keyword;
     // this.setState({searchKeyword: cityName})
     axios.get(`https://restcountries.eu/rest/v2/capital/${cityName}`)
-      .then(res => {
-        name = res.data[0].name
-        timezone = res.data[0].timezones[0]
-        flag = res.data[0].flag
-        currency = res.data[0].currencies[0].code
-        language = res.data[0].languages[0].name
-        capital = res.data[0].capital
-        this.setState({
-          searchResults: res.data[0],
-          isShowingResults: true
-        })
+    .then(res => {
+      name = res.data[0].name
+      timezone = res.data[0].timezones[0]
+      flag = res.data[0].flag
+      currency = res.data[0].currencies[0].code
+      language = res.data[0].languages[0].name
+      capital = res.data[0].capital
+      this.setState({
+        searchResults: res.data[0],
+        isShowingResults: true
       })
-    }
-    render() {
-      const isShowingResults = this.state.isShowingResults
-      if (isShowingResults) {
-        // capital = <Text>{this.state.searchResults.capital}</Text>
-        // name = <Text>{this.state.searchResults.name}</Text>
-        // timezone = <Text>{this.state.searchResults.timezones[0]}</Text>
-        // currency = <Text>{this.state.searchResults.currencies[0].code}</Text>
-        // language = <Text>{this.state.searchResults.languages[0].name}</Text>
-      } else {
-        timezone = ''
-        currency = ''
-        name = ''
-        language = ''
-        capital = ''
+    })
+    
+    
+  }
+  searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 10,
+        term,
+        location:"london"
       }
-      return (
-        <View style={styles.container}>
-        <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: "#F0EEEE", }}>
-          <View style={{ flexDirection: 'row', alignItems: 'center', height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10, flex: 1, borderRadius: 5 }}>
+    });
+    setResults(response.data.businesses)
+    console.log(setResults)
+  };
+
+  render() {
+    
+    const isShowingResults = this.state.isShowingResults
+    if (isShowingResults) {
+      // capital = <Text>{this.state.searchResults.capital}</Text>
+      // name = <Text>{this.state.searchResults.name}</Text>
+      // timezone = <Text>{this.state.searchResults.timezones[0]}</Text>
+      // currency = <Text>{this.state.searchResults.currencies[0].code}</Text>
+      // language = <Text>{this.state.searchResults.languages[0].name}</Text>
+    } else {
+      timezone = ''
+      currency = ''
+      name = ''
+      language = ''
+      capital = ''
+    }
+    return (
+      <View style={styles.container}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: "#F0EEEE",borderColor: 'gray', borderWidth: 0,borderRadius: 10  }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', height: 40, paddingLeft: 10, flex: 1, }}>
             <Feather name='search' style={styles.iconStyle} />
             <TextInput
               style={{ fontSize: 18 }}
@@ -87,9 +105,9 @@ export default class City extends Component {
               returnKeyType="go"
               placeholder="Search for destination"
               onSubmitEditing={() => this.handleSubmit()}
-              // term={term}
-              // onTermChange={setTerm}
-              // onTermSubmit={searchApi}
+            // term={term}
+            // onTermChange={setTerm}
+            onTermSubmit={()=>this.searchApi()}
             />
           </View>
           {/* <Icon.Button
@@ -101,63 +119,62 @@ export default class City extends Component {
           // borderColor='black'
           ></Icon.Button> */}
         </View>
-          {/* <Button
+        {/* <Button
           title="Enter"
           onPress={this.handleSubmit}
         /> */}
-          <Fx />
-          <View style={{ flexDirection: 'row' }}>
-            <MapView
-              style={styles.map}
-              provider={PROVIDER_GOOGLE}
-              initialRegion={{
-                latitude: `${lttd}`,
-                longitude: `${lng}`,
-                latitudeDelta: 20,
-                longitudeDelta: 30,
-              }}
-            />
-            <SvgUri
-              width="10%"
-              height="10%"
-              uri={`${flag}`}
-            />
-            <View style={{ width: 170, padding: 10 }}>
-              <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{capital}</Text>
-              <Text>{capital}</Text>
-              <Text>{name}</Text>
-              <Text>{timezone}</Text>
-              {/* <Text>{currency}</Text> */}
-              <Text>{language}</Text>
-            </View>
+        <Fx />
+        <View style={{ flexDirection: 'row' }}>
+          <MapView
+            style={styles.map}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={{
+              latitude: `${lttd}`,
+              longitude: `${lng}`,
+              latitudeDelta: 20,
+              longitudeDelta: 30,
+            }}
+          />
+          <SvgUri
+            width="10%"
+            height="10%"
+            uri={`${flag}`}
+          />
+          <View style={{ width: 170, padding: 10 }}>
+            <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{capital}</Text>
+            <Text>{capital}</Text>
+            <Text>{name}</Text>
+            <Text>{timezone}</Text>
+            {/* <Text>{currency}</Text> */}
+            <Text>{language}</Text>
           </View>
         </View>
-      );
-    }
+      </View>
+    );
   }
+}
 
-  const styles = StyleSheet.create({
-    container: {
-      justifyContent: 'flex-start',
-      // marginTop: 50,
-      padding: 20,
-      backgroundColor: '#fff',
-      borderRadius: 10,
-      margin: 15,
-      flex: 1.5,
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'flex-start',
+    // marginTop: 50,
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    margin: 15,
+    flex: 1.5,
 
-    },
-    map: {
-      width: 180,
-      height: 150,
-    },
-    iconStyle: {
-      fontSize: 20,
-      color: "gray",
-      alignSelf: "center",
-      marginRight: 10
-    }
-  });
+  },
+  map: {
+    width: 180,
+    height: 150,
+  },
+  iconStyle: {
+    fontSize: 20,
+    color: "gray",
+    alignSelf: "center",
+    marginRight: 10
+  }
+});
 
 
-  
