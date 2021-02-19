61992e9259f061501afb68c80bf2f5fae207ed1b
Ara <hello.ara@pm.me>
2021-02-19 14:47:09 +0000

    multiple flights showing up

--- components/flights.js
+++ components/flights.js
@@ -21,7 +21,9 @@ export default class Flights extends Component {
       flightDate: '',
       link: '',
       dateFrom: moment(new Date()).format("DD/MM/YYYY"),
-      dateTo: moment().add(3, 'months').format('DD/MM/YYYY')
+      dateTo: moment().add(3, 'months').format('DD/MM/YYYY'),
+      numOfFlights: 5,
+      flights: []
     }
   }
 
@@ -60,25 +62,42 @@ export default class Flights extends Component {
 
   searchFlights = () => {
     axios
-      .get(`https://api.skypicker.com/flights?fly_from=${this.state.cityFromCode}&fly_to=${this.state.cityToCode}&dateFrom=${this.state.dateFrom}&dateTo=${this.state.dateTo}&partner=picky&v=3`)
+      .get(`https://api.skypicker.com/flights?fly_from=${this.state.cityFromCode}&fly_to=${this.state.cityToCode}&dateFrom=${this.state.dateFrom}&dateTo=${this.state.dateTo}&sort=duration&limit=${this.state.numOfFlights}&partner=picky&v=3`)
       .then((response) => {
+        let flightsArray = []
         console.log('fetch request was made')
-        let data = response.data.data[0]
-        console.log(data)
-        console.log(Object.keys(data.conversion)[0])
-        console.log(Object.values(data.conversion)[0])
+        let data = response.data.data
+        for (let i = 0; i < data.length; i++) {
+          flightsArray.push({
+            cityFrom: data[i].cityFrom,
+            countryFrom: data[i].countryFrom.name,
+            cityTo: data[i].cityTo,
+            countryTo: data[i].countryTo.name,
+            flightDuration: data[i].fly_duration,
+            currency: Object.keys(data[i].conversion)[0],
+            price: Object.values(data[i].conversion)[0],
+            flightDate: moment(new Date(data[i].aTime * 1000)).format('LLLL'),
+            link: data[i].deep_link
+          })
+        }
         this.setState({
-          cityFrom: data.cityFrom,
-          countryFrom: data.countryFrom.name,
-          cityTo: data.cityTo,
-          countryTo: data.countryTo.name,
-          flightDuration: data.fly_duration,
-          currency: Object.keys(data.conversion)[0],
-          price: Object.values(data.conversion)[0],
-          flightDate: moment(new Date(response.data.data[0].aTime * 1000)).format('LLLL'),
-          link: data.deep_link
+          flights: flightsArray
         })
-        console.log(this.state.countryTo)
+        console.log(this.state.flights)
+
+        // this.setState({
+        //   flights: response.data
+        // //   cityFrom: data.cityFrom,
+        // //   countryFrom: data.countryFrom.name,
+        // //   cityTo: data.cityTo,
+        // //   countryTo: data.countryTo.name,
+        // //   flightDuration: data.fly_duration,
+        // //   currency: Object.keys(data.conversion)[0],
+        // //   price: Object.values(data.conversion)[0],
+        // //   flightDate: moment(new Date(response.data.data[0].aTime * 1000)).format('LLLL'),
+        // //   link: data.deep_link
+        // })
+        // console.log(this.state.countryTo)
         
         // console.log(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`)
         // console.log(moment().format())
@@ -118,14 +137,31 @@ export default class Flights extends Component {
   //   this.searchLandingCityCode()
   //   this.searchFlights()
   // }
-
   render() {
     // this.searchDepartureCityCode();
     // this.searchLandingCityCode();
     // this.searchFlights();
     return (
       <View style={{padding: 20}}>
-        <Text style={{fontSize: 20, fontWeight:'bold'}}>✈️ Flight</Text>
+     
+        <Text >✈️ Flight</Text>
+        {this.state.flights.map((flight,index) => (
+          <View key={index} style={{borderWidth: 1, borderColor:'black', margin: 10}}>
+          <Text>From: {flight.cityFrom}, {flight.countryFrom}</Text>
+          <Text>To: {flight.cityTo}, {flight.countryTo}</Text>
+          <Text>Date: {flight.flightDate}</Text>
+
+          <Text>Duration: {flight.flightDuration}</Text>
+          <Text>{flight.currency}{flight.price}</Text>
+          <Text style={{color: 'blue'}} onPress={()=> Linking.openURL(`${flight.link}`)}>Check out the flight</Text>
+          </View>
+        ))}
+      </View>
+      )
+  }
+}
+
+   {/* <Text style={{fontSize: 20, fontWeight:'bold'}}>✈️ Flight</Text>
 
           <Text>From: {this.state.cityFrom}, {this.state.countryFrom}</Text>
           <Text>To: {this.state.cityTo}, {this.state.countryTo}</Text>
@@ -136,18 +172,3 @@ export default class Flights extends Component {
       <Text style={{color:'blue'}} onPress={()=> Linking.openURL(`${this.state.link}`)}>Check out the flight</Text>
       {/* <Text>{this.state.dateFrom}</Text>
       <Text>{this.state.dateTo}</Text> */}
-      </View>
-      )
-    // countryFrom: '',
-    // cityTo: '',
-    // CountyTo: '',
-    // flightTime: '',
-    // flightDuration: '',
-    // price: '',
-    // flightDate: '',
-    // link: '',
-    // dateFrom: moment(new Date()).format("YYYY/MM/DD"),
-    // dateTo: console.log(moment().add(3, 'months').format('YYYY/MM/DD'))}</Text>
-  }
-}
-
